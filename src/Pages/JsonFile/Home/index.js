e67d28b75708573eb9567c268b34b7892b501a50
ProductionCode <130960660+techcode34@users.noreply.github.com>
2025-03-02 11:17:23 +0800

    added the image file
    added the image file

--- src/Pages/JsonFile/Home/index.js
+++ src/Pages/JsonFile/Home/index.js
@@ -1,6 +1,12 @@
-import AttendanceImg from "../../../Assets/images/jpg/image1.jpg"
-import GroupPeople from "../../../Assets/svg/group-people-working-team.svg"
-import LoginSystem from "../../../Assets/svg/loginScreenImage.svg"
+import HomeImage from "../../../Assets/images/png/HomeImage.png"
+
+import ResidentialPaintingImage from "../../../Assets/images/png/ResidentialPaintingImage.png"
+import CommercialPaintingImage from "../../../Assets/images/png/CommercialPaintingImage.png"
+import CorporateStrataPaintingImage from "../../../Assets/images/png/CorporateStrataPaintingImage.png"
+
+import customerService from "../../../Assets/images/png/customerService.png"
+import affortableCost from "../../../Assets/images/png/affortableCost.png"
+
 
 
 export const homePgContent = [
@@ -17,16 +23,16 @@ export const homePgContent = [
 +"Trust us to handle every detail, with quotes provided for each project, ensuring great value for your investment" }],
         btnLabel: "View in detail",
         button: true,
-        secondContentImg: AttendanceImg,
+        secondContentImg: HomeImage,
         firstContentImg: "",
         imgClass: "first_att_img",
         ecllips: true
     },
     {
         headingStyle: "sec_main_heading",
-        title: "In the world of challenges, we offer solution",
-        description: [{ para: "GPaint has a strong motive of providing better solutions for various problems exist around. In a world where challenges often seem insurmountable, we stand firm as your ally, ready to face adversity head-on. We believe that every obstacle is an opportunity for innovation. Here's why we are your go-to partner for solutions in the face of challenges." }],
-        firstContentImg: GroupPeople,
+        title: "Excellent Customer Service",
+        description: [{ para: "Your satisfaction is our top priority. We listen to your needs, provide honest advice, and keep you informed at every stage of the project." }],
+        firstContentImg: customerService,
         secondContentImg: "",
         button: false,
         imgClass: "grp_img"
@@ -34,10 +40,11 @@ export const homePgContent = [
     },
     {
         headingStyle: "sec_main_heading",
-        bottomSubTitle: "Single account for multiple products",
-        title: "Unified Account Access",
-        description: [{ para: "Gone are the days of juggling multiple usernames and passwords. With Unified Account Access, you can streamline your digital life by managing all your services and platforms from a single, secure account. Whether you're accessing our hiring platform, live attendance system — it's all at your fingertips." }, { para: "Join us in embracing unity, efficiency, and a future where your online experience works harmoniously." }],
-        secondContentImg: LoginSystem,
+        // bottomSubTitle: "Single account for multiple products",
+        title: "Affordable Pricing with No Hidden Costs",
+        description: [{ para: "We offer competitive pricing with detailed quotes – no surprises, just great value for money." },
+             { para: "Join us in embracing unity, efficiency, and a future where your online experience works harmoniously." }],
+        secondContentImg: affortableCost,
         firstContentImg: "",
         button: false,
         bottomSubtittleClass: "bottom_subtittle",
@@ -48,34 +55,57 @@ export const homePgContent = [
 export const exploreProduct = [
     {
         maintitle: "Explore our product",
-        subTitle: "Be precised in attendance",
-        para: "Live Attendance Platform is designed with innovative Boundary Check-In feature, which helps you draw boundaries of your organization for precised check-in and check-out",
+        subTitle: "Residential Painting",
+        para: "Transform your home with our expert interior and exterior painting services. We bring a fresh look to your living spaces with vibrant colours and smooth finishes  all tailored to your style."
+        + "✔ New Homes ✔ Repaints & Renovations ✔ Feature Walls ✔ Texture Coating✔ Fence & Deck Painting",
         buttonLabel: "Get Started",
         navLink: "/",
-        img: AttendanceImg
+        img: ResidentialPaintingImage
+    },
+
+
+    {
+        maintitle: "Explore our product",
+        subTitle: "Commercial Painting",
+        para: "We understand how important it is to maintain a professional image for your business. Our commercial painting services will give your property a clean, fresh, and inviting look to impress your customers and employees."
+        + "✔ Offices ✔ Shops & Retail Outlets ✔ Restaurants ✔ Warehouses ✔ Industrial Buildings",
+        buttonLabel: "Get Started",
+        navLink: "/",
+        img: CommercialPaintingImage
+    },
+
+    {
+        maintitle: "Explore our product",
+        subTitle: "Corporate & Strata Painting",
+        para: "We provide high-quality painting solutions for corporate offices, apartments, and strata buildings. Our team works efficiently to ensure minimal disruption to your business operations."
+        + "✔ ✔ Corporate Offices ✔ Apartment Complexes & Common Area ✔ High-Rise Buildings ✔ Strata Maintenance",
+        buttonLabel: "Get Started",
+        navLink: "/",
+        img: CorporateStrataPaintingImage
     }
+
+
 ];
 
 export const subscriptionDetail = [{
-    maintitle: "Subscription",
-    para: "Join our live attendance platform today and enjoy the advantages of an exclusive subscription plan",
+    maintitle: "Payment Plans",
+    para: "Choose the best plan for your painting project and enjoy professional service with flexible payment options. Whether it's a one-time project or an ongoing contract, we've got you covered.",
     amountdetails: [
         {
-            heading: "Per User",
-            para: "This is the total number of users who can check-in per day through Check-in App.",
-            rate: "$3"
+            heading: "Contract Work",
+            para: "For larger or ongoing projects, we offer customized contracts based on your specific needs. This plan includes a detailed agreement and a fixed price for the entire project.",
+            // rate: "$3"
         },
         {
-            heading: "Per Space",
-            para: "This is the number of spaces you can create after subscription.",
-            rate: "$27"
+            heading: "Hourly Work",
+            para: "If you need a smaller project or just a few hours of work, our hourly service is perfect for you. You only pay for the time spent on your project.",
+            // rate: "$27"
         }
     ],
-    boldtext: "Subscription amount will be the sum of both space and total users count",
-    paraheading: "Subscription period",
-    paraLine: "1 month to 1 year",
-    para1: "(This doesn’t include any charge. Users can choose any subscription period)",
-    para2: "Auto-renewal applies for 1 month to 6 months of plan (Auto renewal can be done till a year completes, new subscription has to be done every year to start auto renewal again) The plan over 6 months of subscription has to be manually renewed."
+    // boldtext: "Subscription amount will be the sum of both space and total users count",
+    paraheading: "Flexible Payment Options",
+    paraLine: "Choose the plan that fits your needs and budget.",
+    para2: "Payment Methods Accepted:Bank Transfer, Cash Payment Contact us today to discuss your project and choose the best payment plan for your needs!"
 
 }]
 
